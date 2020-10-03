f636c10e1c087cbd699b70a1d95c8beba8bc4479
johansab <johansabouret@gmail.com>
2020-10-03 17:58:29 -0400

    First test

---
+++ anotherDay/Assets/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //reference to the rigidbody component "rb"
+    public Rigidbody rb;
+
+    public Transform Player;
+    
+    //reference to the direction
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 2000f;
+    public float jumpForce = 2000f;
+    public bool grounded = true;
+ 
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Menu
+        Move();
+        
+
+    }
+
+    private void Move()
+    {
+
+        //Add a forward force in 5 directions
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        else if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime, ForceMode.VelocityChange);
+        }
+        else if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        else if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -forwardForce * Time.deltaTime, ForceMode.VelocityChange);
+        }
+        else if (Input.GetKey("space") && grounded == true)
+        {
+            rb.AddForce(new Vector3(0, 6, 0), ForceMode.Impulse);
+            grounded = false;
+        }
+        else if (Player.position.y == 1)
+        {
+            grounded = true;
+        }
+        
+    }
+}
---
+++ anotherDay/Assets/zombie.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+public class zombie : MonoBehaviour
+{
+
+    public Transform PosJoueur;
+    public float SpeedEnemy = 2f;
+    public Rigidbody bodyZombie;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Direction Point A à B
+        var direction = PosJoueur.position - transform.position;
+        var angle = Vector3.Angle(direction, this.transform.forward);
+
+
+        if (angle < 180 && direction.magnitude < 12)
+        {
+            var rotation = Quaternion.Slerp(
+                this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime);
+            rotation.x = 0;
+            rotation.z = 0;
+            transform.rotation = rotation;
+
+            if (direction.magnitude > 1)
+                transform.Translate(0, 0, Time.deltaTime * SpeedEnemy);
+            
+        }
+    }
+}
