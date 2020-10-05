ce30f7e32d0e8bf867d5809f87d0f6a33682fcdd
Louis <louis.pougis@gmail.com>
2020-10-05 18:15:30 -0400

    First try for jump
    Check if already grounded before allowing the player to jump

---
+++ anotherDay/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    public PlayerMovement movement;
+    public bool grounded = false;
+
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Floor")
+        {
+            grounded = true;
+        }
+        else { grounded = false; }
+    }
+}
--- anotherDay/Assets/Scripts/PlayerMovement.cs
+++ anotherDay/Assets/Scripts/PlayerMovement.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -9,52 +6,67 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody rb;
 
     public Transform Player;
+    public PlayerCollision playerCollision;
     
     //reference to the direction
     public float forwardForce = 2000f;
     public float sidewaysForce = 2000f;
     public float jumpForce = 2000f;
-    public bool grounded = true;
- 
-
+    public int accelerationValue = 3000;
+    //public bool grounded = true;
+   
     // Update is called once per frame
     void FixedUpdate()
     {
         //Menu
         Move();
         
-
     }
 
     private void Move()
     {
 
         //Add a forward force in 5 directions
-        if (Input.GetKey("d"))
+        if (Input.GetKey(KeyCode.D))
         {
             rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
-        else if (Input.GetKey("w"))
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(new Vector3(0, 0, 3000 * Time.deltaTime), ForceMode.Acceleration + 3000);
+        }
+        else if (Input.GetKey(KeyCode.W))
         {
             rb.AddForce(0, 0, forwardForce * Time.deltaTime, ForceMode.VelocityChange);
         }
-        else if (Input.GetKey("a"))
+        else if (Input.GetKey(KeyCode.A))
         {
             rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
-        else if (Input.GetKey("s"))
+        else if (Input.GetKey(KeyCode.S))
         {
             rb.AddForce(0, 0, -forwardForce * Time.deltaTime, ForceMode.VelocityChange);
         }
-        else if (Input.GetKey("space") && grounded == true)
+        //test saut sur place
+        if (Input.GetKey(KeyCode.Space) && playerCollision.grounded)
         {
             rb.AddForce(new Vector3(0, 6, 0), ForceMode.Impulse);
-            grounded = false;
+            Debug.Log("CA NE PLANTE PAAAAAS");
         }
-        else if (Player.position.y == 1)
+        /*else if (Player.position.y == 1)
         {
-            grounded = true;
-        }
-        
+            playerCollision.setGround(true);
+        }*/
+
+        if (Input.GetKey(KeyCode.LeftShift)  && Input.GetKey(KeyCode.W))
+    {
+        rb.AddForce(new Vector3(0, 0, Time.deltaTime), ForceMode.Acceleration + accelerationValue);
+    }
+    if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.Space) && playerCollision.grounded)
+    {
+            rb.AddForce(new Vector3(0, 6, 0), ForceMode.Impulse);
+            Debug.Log("CA NE PLANTE PAAAAAS");
+    }
+
     }
 }
