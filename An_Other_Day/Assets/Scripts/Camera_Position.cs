416961bbe0266e25a1b2f308b0cd7e9861ee5097
Sabouret Johan <johansabouret@gmail.com>
2020-10-10 18:47:48 -0400

    Camera
    First camera + Pivot
    Checkboard (shader)

---
+++ An_Other_Day/Assets/Scripts/Camera_Position.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Camera_Position : MonoBehaviour
+{
+    public Transform Player1;
+    public Vector3 _offset;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = Player1.position + _offset;
+    }
+}
