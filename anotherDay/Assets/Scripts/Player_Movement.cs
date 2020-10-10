5b7161778b94d93bc96a1e12069a7addc93acac0
Sabouret Johan <johansabouret@gmail.com>
2020-10-10 03:04:46 -0400

    Movement player
    work in progress

--- anotherDay/Assets/Scripts/Player_Movement.cs
+++ anotherDay/Assets/Scripts/Player_Movement.cs
@@ -12,12 +12,26 @@ public class Player_Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Movement
+
+        //Input in certain direction
+        //Value between -1 and 1 = Horizontal Input
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
+
+        //Vector3 to point to a direction
+        //mooving with x and z axis
+        //normalized for diagonal 
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
+        //check the direction
+        //get Input to move
         if(direction.magnitude >= 0.1f)
         {
+           // Calculate diagonale
+            float targetAngle = Mathf.Atan2(direction.x, direction.y);
+
+            //Time.deltaTime frame rate independant
             controller.Move(direction*speed*Time.deltaTime);
         }
     }
